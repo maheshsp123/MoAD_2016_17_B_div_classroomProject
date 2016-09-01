504bb2ef440ffeead79c9b19b8b70579422e8584
maheshsp123 <maheshsp123@gmail.com>
2016-09-01 09:57:14 +0530

    commit3 1/9/2016

--- app/src/main/java/bvb/android/example/com/moad_2016_17/loginActivity.java
+++ app/src/main/java/bvb/android/example/com/moad_2016_17/loginActivity.java
@@ -2,7 +2,9 @@ package bvb.android.example.com.moad_2016_17;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +16,7 @@ public class loginActivity extends AppCompatActivity {
     EditText passwd_ref;
     Button submitBtn_ref;
     UserCrendentials[] userArray=new UserCrendentials[4];
+    String LOG_CAT=loginActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,7 +54,20 @@ public class loginActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_login,menu);
+        getMenuInflater().inflate(R.menu.menu_login,menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int selected_item= item.getItemId();
+        if(selected_item==R.id.profileId)
+        {
+            Log.i(LOG_CAT,"profile menu item selected");
+            Toast.makeText
+                    (loginActivity.this, "profile menu item",
+                            Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
