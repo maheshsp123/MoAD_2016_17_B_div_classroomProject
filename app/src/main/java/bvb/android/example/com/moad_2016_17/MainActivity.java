4c2d085540564eb90a3148016ef92226f54a9e4d
mahesh <maheshsp123@gmail.com>
2016-09-17 11:41:32 +0530

    commit cabin pc 1 17-9-2016

--- app/src/main/java/bvb/android/example/com/moad_2016_17/MainActivity.java
+++ app/src/main/java/bvb/android/example/com/moad_2016_17/MainActivity.java
@@ -2,20 +2,18 @@ package bvb.android.example.com.moad_2016_17;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     String LOG_CAT= MainActivity.class.getSimpleName();
     AppCompatActivity thisActivityReference;
     Button fourthActivityButtonRef;
+    //test line from cabin pc for GitHub
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.i(LOG_CAT, "onCreate()");
